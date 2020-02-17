766165030e1914cd653f6b46399a5621928af10b
Gourge Altawil <joetawil7@hotmail.com>
2020-02-17 19:33:51 +0100

    small changes

--- src/main/java/com/es/example/flight/FlightController.java
+++ src/main/java/com/es/example/flight/FlightController.java
@@ -1,7 +1,6 @@
 package com.es.example.flight;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
--- src/main/java/com/es/example/flight/FlightServiceImpl.java
+++ src/main/java/com/es/example/flight/FlightServiceImpl.java
@@ -1,15 +1,10 @@
 package com.es.example.flight;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.OffsetDateTime;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class FlightServiceImpl implements FlightService {
